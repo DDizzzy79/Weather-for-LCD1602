a0334d51a8417aff78f40296fccd046dff07e5a4
Retr0mous <72267897+DDizzzy79@users.noreply.github.com>
2021-11-29 19:40:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,22 @@ import smbus
 import time
 import sys
 import LCD1602 as LCD
-import weather as wea
+import weather1 as wea
 if __name__ == '__main__':
     temp = wea.RetWea()
-    weather = "The temperature is",temp,"now"
     LCD.init_lcd()
     time.sleep(1)
     LCD.turn_light(1)
+    LCD.print_lcd(0,0,"It is " + temp + "C" )
     while True:
-        LCD.print_lcd(1, 1, weather)
+        nowtime = time.strftime('%m-%d %H:%M:%S',time.localtime(time.time()))
+        hourtime = time.strftime('%H',time.localtime(time.time()))
+        mintime = time.strftime('%M',time.localtime(time.time()))
+        sectime = time.strftime('%S',time.localtime(time.time()))
+        LCD.print_lcd(1,1,nowtime)
+        if mintime == '59':
+            if sectime == '00':
+                LCD.turn_light(1)
+            elif sectime  == '59':
+                LCD.turn_light(0)
         time.sleep(0.2)
