34020408337c12ce772b3a4f482a411a16e40051
DDizzzy79 <Patrick_0429@163.com>
2021-11-29 18:58:26 +0800

    Adding Weather.py

---
+++ weather.py
@@ -0,0 +1,17 @@
+import requests
+import pprint
+Key = "&key=" + ""          #input keys
+CityName = ""
+def Getcity(CityName):
+    url_v2 = "https://geoapi.qweather.com/v2/city/lookup?location=" + CityName + Key
+    CityArg =  requests.get(url_v2).json()['location'][0]
+    return CityArg["id"]
+    
+    
+def GetInfo(location):
+    url = "https://devapi.qweather.com/v7/weather/now?" + Key + "&location=" + location
+    return requests.get(url).json()
+CityId = Getcity(CityName)
+WeatherNow = GetInfo(CityId)
+print("It is: " + WeatherNow['now']['temp'] + " degree")
+    
